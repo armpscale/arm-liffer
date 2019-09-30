72207e3a36797f890380912306fcf150faec1028
Akkrawut Khanthawan <akkrawut33588@gmail.com>
2019-10-01 02:48:51 +0700

    Edit RandomCard component for use css module

--- src/components/RandomCard/cards.js
+++ src/components/RandomCard/cards.js
@@ -1,13 +1,13 @@
 export const cardAt = (number) => {
-  let cardType = ["C","D","H","S"];
-  let cardNum = ["2","3","4","5","6","7","8","9","0","J","Q","K","A"];
-  
-  if (number < 0 || number > (cardType.length * cardNum.length) - 1) {
-      return `Number cannot < 0 or > ${(cardType.length * cardNum.length) - 1}`
-  }  
+    let cardType = ["C", "D", "H", "S"];
+    let cardNum = ["2", "3", "4", "5", "6", "7", "8", "9", "0", "J", "Q", "K", "A"];
 
-  let indexCardNum = number % cardNum.length
-  let indexCardType = parseInt(number / cardNum.length)
+    if (number < 0 || number > (cardType.length * cardNum.length) - 1) {
+        return `Number cannot < 0 or > ${(cardType.length * cardNum.length) - 1}`
+    }
 
-  return cardNum[indexCardNum] + cardType[indexCardType]
+    let indexCardNum = number % cardNum.length
+    let indexCardType = parseInt(number / cardNum.length)
+
+    return cardNum[indexCardNum] + cardType[indexCardType]
 };
--- src/components/RandomCard/index.jsx
+++ src/components/RandomCard/index.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './Card.module.css';
+import styles from './Card.module.css';
 import { cardAt } from './cards';
 
 import images from './lineRich.png';
@@ -17,12 +17,12 @@ class RandomCard extends Component {
   render() {
 
     return (
-      <div class="flip-card">
-        <div class="flip-card-inner">
-          <div class="flip-card-front">
+      <div className={styles["flip-card"]}>
+        <div className={styles["flip-card-inner"]}>
+          <div className={styles["flip-card-front"]}>
             <img src={images} alt="images" width={300} height={300} />
           </div>
-          <div class="flip-card-back">
+          <div className={styles["flip-card-back"]}>
             <h1>ArmAeng</h1>
             <p>We love that guy</p>
           </div>
