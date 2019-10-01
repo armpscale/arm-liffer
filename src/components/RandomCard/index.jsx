258743847f9d0e9432c6af36e6ee3d7ac11101a5
Akkrawut Khanthawan <akkrawut33588@gmail.com>
2019-10-02 02:48:18 +0700

    Refactor RandomCard component for use hook

--- src/components/RandomCard/index.jsx
+++ src/components/RandomCard/index.jsx
@@ -1,35 +1,121 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import styles from './Card.module.css';
 import { cardAt } from './cards';
 
-import images from './lineRich.png';
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Paper from '@material-ui/core/Paper';
+import Divider from '@material-ui/core/Divider';
 
-class RandomCard extends Component {
+import club from './clubs.png';
+import diamond from './diamonds.png';
+import heart from './hearts.png';
+import spade from './spades.png';
 
-  constructor(props) {
-    super(props);
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(1.5),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+  divider: {
+    margin: `30px 30px 30px ${theme.spacing(5)}px`,
+  },
+  text: {
+    textAlign: 'center',
+    verticalAlign: 'baseline',
+  },
+}));
 
-    this.state = {
-      randomNum: 0,
-      cardName: "",
-    };
+export default function RandomCard() {
+  const classes = useStyles();
+  const [number, random] = useState(0);
+
+  function randomNumber() {
+    const min = 0;
+    const max = 51;
+    return parseInt(min + Math.random() * (max - min));
   }
-  render() {
 
-    return (
-      <div className={styles["flip-card"]}>
-        <div className={styles["flip-card-inner"]}>
-          <div className={styles["flip-card-front"]}>
-            <img src={images} alt="images" width={300} height={300} />
-          </div>
-          <div className={styles["flip-card-back"]}>
-            <h1>ArmAeng</h1>
-            <p>We love that guy</p>
+  function separateCardName(number) {
+    let cardName = cardAt(number);
+    return [cardName.charAt(0), cardName.charAt(1)];
+  }
+
+  return (
+    <div className={classes.root}>
+      <Grid
+        container
+        direction="column"
+        justify="flex-start"
+        alignItems="center"
+      >
+        <div className={styles["flip-card"]}>
+          <div className={styles["flip-card-inner"]}>
+            <div className={styles["flip-card-front"]}>
+              <h1 style={{verticalAlign: 'baseline'}}>ArmAeng</h1>
+            </div>
+            <div className={styles["flip-card-back"]}>
+              <h1 className={styles["text-padding-left"]}>
+                {separateCardName(number)[0]}
+              </h1>
+              {(() => {
+                switch (separateCardName(number)[1]) {
+                  case 'C':
+                    return <img src={club} alt="club" width={100} height={100} />;
+                  case 'D':
+                    return <img src={diamond} alt="diamond" width={100} height={100} />;
+                  case 'H':
+                    return <img src={heart} alt="heart" width={100} height={100} />;
+                  case 'S':
+                    return <img src={spade} alt="spade" width={100} height={100} />;
+                  default:
+                    return null;
+                }
+              })()}
+              <h1 className={styles["text-padding-right"]}>
+                {separateCardName(number)[0]}
+              </h1>
+            </div>
           </div>
         </div>
-      </div>
-    )
-  }
-}
+      </Grid>
 
-export default RandomCard;
+      <Grid
+        container
+        direction="column"
+        justify="flex-start"
+        alignItems="center"
+        spacing={3}
+      >
+        <Grid item xs={12}>
+          <Button
+            variant="contained"
+            color="default"
+            onClick={() => random(randomNumber())}
+          >
+            Random
+        </Button>
+        </Grid>
+      </Grid>
+
+      <Divider className={classes.divider} />
+
+      <Grid
+        container
+        justify="center"
+        alignItems="center"
+        spacing={3}
+      >
+        <Grid item xs={3}>
+          <Paper className={classes.paper}>Card Number : {number} || Card Name : {separateCardName(number)}</Paper>
+        </Grid>
+      </Grid>
+    </div >
+
+  )
+}
